1001d1bd813c12fb3a492b4bada746786a4d3130
hallerite <git@hallerite.com>
2026-01-14 19:53:30 +0100

    add Prime Hub integration

---
+++ integrations/verifiers/vf_sere.py
@@ -0,0 +1,8 @@
+"""vf-sere module for Verifiers framework compatibility.
+
+This module provides the required module name (vf_sere) for the vf-sere package.
+"""
+
+from integrations.verifiers import load_environment
+
+__all__ = ["load_environment"]
